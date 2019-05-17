10e746c9ac95ee2efe5b4183dfc4bdfb02f2d753
Stefan <Stefan.Demary@t-online.de>
2019-05-17 11:16:39 +0200

    updated test classes for junit5

--- test/java/utils/CharacterModifierTest.java
+++ test/java/utils/CharacterModifierTest.java
@@ -1,14 +1,13 @@
 package utils;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
 
 public class CharacterModifierTest {
-	
+
 
 	@Test
-	void simpleTest(){
+	void simpleTest() {
 
 	}
 
